720e785b101282a9606c0e7c3eb732da6804c703
G1ue <73698877+G1ue@users.noreply.github.com>
2020-11-04 15:11:02 +0900

    Update Application.cpp

--- Application.cpp
+++ Application.cpp
@@ -110,7 +110,7 @@ int Application::GetCommand() {
 }
 
 
-// Add a new record into the contents list and the event list. (to the favorite list is also possible)
+// Add a new record into the contents list and the event list. 
 int Application::AddContents() {
 
 	if (m_ContentsList.IsFull()) // If the list is full, don't add a new record, 
@@ -573,3 +573,7 @@ void Application::FindNDisplayByEventAndPerson() {
 		cout << "<======== ITEM Not Found!==========>" << endl;
 	}
 }
+
+
+// Consider the state of the gallery search and my favorite gallery after deletion in my gallery. 
+// Change the expression 'Research' into -> 'Search' 
